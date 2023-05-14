2659248a684b1aa8903be5d5ff77e968a2be4f09
Alexandr Vinnyk <124481298+alexvinnyk@users.noreply.github.com>
2023-05-14 16:09:25 -0500

    Update PracticeStart_2.java
    Added new commit

--- src/PracticeStart_2.java
+++ src/PracticeStart_2.java
@@ -2,5 +2,5 @@ public class PracticeStart_2 {
    // In the morning the sun will wake you up with its rays.;
    // the rising sun will absorb the shadow.;
 
-
+//at the same time , the Sundoes not forget to smile
 }
